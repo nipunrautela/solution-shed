d4fcde777290a6af089d4cb9a60ae65b629f5627
Nipun Rautela <nipunrautela261@gmail.com>
2024-10-31 00:11:50 +0530

    Backend springboot initialization

---
+++ solutionshed/src/main/java/com/nipunrautela/solutionshed/SolutionShedApplication.java
@@ -0,0 +1,13 @@
+package com.nipunrautela.solutionshed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionShedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionShedApplication.class, args);
+	}
+
+}
